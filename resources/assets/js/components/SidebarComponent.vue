5beba01ebdfd409397521846d72da537932c6ed2
Antonio Ramirez <ajrmzcs@gmail.com>
2018-01-28 01:13:15 -0500

    Project added to sidebar

--- resources/assets/js/components/SidebarComponent.vue
+++ resources/assets/js/components/SidebarComponent.vue
@@ -22,7 +22,7 @@
                 <h4 class="panel-title">Projects</h4>
             </div>
             <div class="panel-body">
-
+                <a href="https://github.com/ajrmzcs/antonioramirez.co">This site Github Repository</a>
             </div>
         </div>
     </div>
